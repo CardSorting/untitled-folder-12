38fb3202431e37aa3e544d0e38ee3c9cb5a59a79
PlayMoreTCG <playmoretcg@gmail.com>
2024-12-21 16:06:13 -0700

    continuedwebpackerror

--- chrome-extension/webpack.config.js
+++ chrome-extension/webpack.config.js
@@ -4,39 +4,27 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   entry: {
-    content: './src/content.ts',
     background: './src/background.ts',
+    content: './src/content.ts',
     textProcessorWorker: './src/textProcessorWorker.ts'
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true // This will clean the output directory before each build
+    path: path.resolve(__dirname, '.'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              compilerOptions: {
-                module: 'es2015',
-                moduleResolution: 'node'
-              }
-            }
-          }
-        ],
-        exclude: /node_modules/
-      }
-    ]
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
-    modules: ['node_modules']
   },
   experiments: {
-    topLevelAwait: true
-  }
+    topLevelAwait: true,
+  },
 };
